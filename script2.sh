f7e6745cdb171e74b760a928db8eaf7dc5ef571c
Priyanka Singh <188777878+Piku005-sng@users.noreply.github.com>
2026-03-22 22:11:51 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 # Script 2: FOSS Package Inspector
 # Author: Priyanka Singh
+# This script checks if a package is installed
 
-PACKAGE="python3"
+PACKAGE="python3"   # Package to check
 
 echo "Checking package: $PACKAGE"
 
+# --- Check if package is installed ---
 if dpkg -l | grep -qw $PACKAGE; then
     echo "$PACKAGE is installed."
-    dpkg -l | grep $PACKAGE
+    dpkg -l | grep $PACKAGE   # Show package details
 else
     echo "$PACKAGE is NOT installed."
 fi
 
+# --- Print description using case statement ---
 case $PACKAGE in
-    python3) echo "Python: simple, powerful, and community-driven language" ;;
-    apache2) echo "Apache: backbone of web servers" ;;
-    mysql) echo "MySQL: open-source database system" ;;
-    vlc) echo "VLC: universal media player" ;;
+    python3) echo "Python: simple, powerful, and open-source language" ;;
+    apache2) echo "Apache: web server software" ;;
+    mysql) echo "MySQL: database system" ;;
+    vlc) echo "VLC: media player" ;;
     *) echo "Unknown package" ;;
 esac
